ae753bb8cbdc3f3a4e05ccab656b22ce89660367
estebanGeorgebrown <joseesteban.guzmanalvarez@georgebrown.ca>
2020-03-16 02:37:48 -0400

    Fixed getOnePost

--- linkUp-functions/functions/handlers/posts.js
+++ linkUp-functions/functions/handlers/posts.js
@@ -45,3 +45,34 @@ exports.getAllPosts = (request, response) => {
 
 
  }
+
+ //Get one post
+
+ exports.getPost = (req, res) => {
+    let postData = {};
+    db.doc(`/posts/${req.params.postId}`)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          return res.status(404).json({ error: 'Post not found' });
+        }
+        postData = doc.data();
+        postData.postId = doc.id;
+        return db
+          .collection('comments')
+          .orderBy('createdAt', 'desc')
+          .where('postId', '==', req.params.postId)
+          .get();
+      })
+      .then((data) => {
+        postData.comments = [];
+        data.forEach(doc => {
+          postData.comments.push(doc.data());
+        });
+        return res.json(postData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: err.code });
+      });
+  };
