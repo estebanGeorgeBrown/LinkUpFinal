4f6bebf75ecfe8c07cb7fc9439af8e4f36e4fd67
Alejandro <Alejandro.Gutierrez@georgebrown.ca>
2020-04-11 18:59:14 -0400

    Theme fixed -Alejandro

--- linkup-client/src/pages/login.jsx
+++ linkup-client/src/pages/login.jsx
@@ -12,32 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-  form: {
-    textAlign: 'center'
-  },
-  image: {
-    margin: '20px auto 20px auto'
-  },
-  pageTitle: {
-    margin: '10px auto 10px auto'
-  },
-  textField: {
-    margin: '10px auto 10px auto'
-  },
-  button: {
-    marginTop: 20,
-    position: 'relative'
-  },
-  customError:{
-    color: 'red',
-    fontSize: '0.8rem',
-    marginTop: 10
-  },
-  progress: {
-    position: 'absolute'
-  }
-};
+const styles = (theme) => ({
+  ...theme.spreadThis
+})
 
 export class login extends Component {
   constructor(){
@@ -57,17 +34,20 @@ export class login extends Component {
     const userData = {
       email: this.state.email,
       password: this.state.password
-    };
+    }
+    axios.defaults.baseURL =
+    'https://us-central1-linkup-ed6c5.cloudfunctions.net/api';
     axios
       .post('/login', userData)
-      .then(res =>{
+      .then((res)=>{
         console.log(res.data);
+        localStorage.setItem('FBIdToken', 'Bearer ${res.data.token}');
         this.setState({
           loading: false
       });
       this.props.history.push('/');
     })
-    .catch(err => {
+    .catch((err) => {
       this.setState({
         errors: err.response.data,
         loading: false
