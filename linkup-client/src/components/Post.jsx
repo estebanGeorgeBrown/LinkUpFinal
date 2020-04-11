af2c8f5f66b03b4503b7b1f95f75724a0fd553bb
estebanGeorgebrown <joseesteban.guzmanalvarez@georgebrown.ca>
2020-04-11 00:31:27 -0400

    Fixed Date

--- linkup-client/src/components/Post.jsx
+++ linkup-client/src/components/Post.jsx
@@ -1,39 +1,45 @@
-import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles';
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 // MUI Stuff
 
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
-    card: {
-        display: 'flex'
-    }
-}
+  card: {
+    display: "flex",
+  },
+};
 export class Post extends Component {
-
-    render() {
-
-        const { classes, post : {body, createdAt, userImage, userHandle, postId, likeCount, commentCount} } = this.props
-        return (
-            <Card>
-                <CardMedia
-                image={userImage}
-                title="Profile image"/>
-                <CardContent>
-                    <Typography variant="h5">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">
-                    {createdAt.toString()}
-                    </Typography> 
-                    <Typography variant="body1">{body}</Typography>
-                </CardContent>
-            </Card>
-        );
-    }
+  render() {
+    const {
+      classes,
+      post: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        postId,
+        likeCount,
+        commentCount,
+      },
+    } = this.props;
+    return (
+      <Card>
+        <CardMedia image={userImage} title="Profile image" />
+        <CardContent>
+          <Typography variant="h5">{userHandle}</Typography>
+          <Typography variant="body2" color="textSecondary">
+            {new Date(createdAt._seconds * 1000).toLocaleDateString("en-US")}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
 export default withStyles(styles)(Post);
