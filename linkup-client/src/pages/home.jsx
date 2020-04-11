e96f30d00c4598a122b4714ab71c87e5dda20183
Alejandro <Alejandro.Gutierrez@georgebrown.ca>
2020-04-10 23:42:03 -0400

    Adding post component -Alejandro

--- linkup-client/src/pages/home.jsx
+++ linkup-client/src/pages/home.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import Grid  from "@material-ui/core/Grid";
 import axios from 'axios'; 
-
-
+import Post from '../components/Post';
 export class home extends Component {
   state = {
     posts: null
   }
+  
   componentDidMount(){
+    axios.defaults.baseURL =
+  'https://us-central1-linkup-ed6c5.cloudfunctions.net/api';
     axios.get('/posts')
     .then(res => {
       console.log(res.data)
@@ -19,8 +21,10 @@ export class home extends Component {
   }
   render() {
     let recentPostsMarkup = this.state.posts ? (
-    this.state.posts.map(posts => <p>{posts.body}</p>)
-    ): <p>Loading...</p>
+    this.state.posts.map((post) => <Post post={post}/>)
+    ):( 
+      <p>Loading...</p>
+      );
     return (
       <Grid container spacing={2}>
         <Grid item sm ={8} xs={12}>
