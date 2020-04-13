f457153058b6ed1c25058cf52ecceba87aebbf0f
Alejandro <Alejandro.Gutierrez@georgebrown.ca>
2020-04-13 17:26:09 -0400

    fixed working redux

--- linkup-client/src/redux/store.js
+++ linkup-client/src/redux/store.js
@@ -5,16 +5,22 @@ import userReducer from './reducers/userReducer';
 import dataReducer from './reducers/dataReducer';
 import uiReducer from './reducers/uiReducer';
 
-const initialState ={};
+const initialState = {};
 
 const middleware = [thunk];
 
 const reducers = combineReducers({
-    user: userReducer,
-    data: dataReducer,
-    UI: uiReducer
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
 });
 
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const store = createStore(reducers, initialState, enhancer);
 
 export default store;
