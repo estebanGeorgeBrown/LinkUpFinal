cacb9b34b3a05b3f79c03992fbe5defc0164ca4b
Alejandro <Alejandro.Gutierrez@georgebrown.ca>
2020-04-13 20:16:42 -0400

    implementing redux on components- Alejandro

--- linkup-client/src/pages/login.jsx
+++ linkup-client/src/pages/login.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/logo192.png';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 //MUI Stuff
@@ -28,6 +27,11 @@ export class login extends Component {
       errors: {}
     };
   }
+  componentWillReceiveProps(nextProps){
+    if (nextProps.UI.errors){
+    this.setState({ errors: nextProps.UI.errors});
+  }
+}
   handleSubmit = (event) => {
     event.preventDefault();
     const userData = {
