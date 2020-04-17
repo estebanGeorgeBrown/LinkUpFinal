2058e314228df7540356b43afe28f1681284ad7a
estebanGeorgebrown <joseesteban.guzmanalvarez@georgebrown.ca>
2020-04-16 20:22:19 -0400

    Added delete button

--- linkup-client/src/components/Post.jsx
+++ linkup-client/src/components/Post.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import {Link} from 'react-router-dom';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import PropTypes from 'prop-types';
-import MyButton from '../util/MyButton';
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
+import DeletePost from "./DeletePost";
 
 // MUI Stuff
 
@@ -15,9 +16,9 @@ import Typography from "@material-ui/core/Typography";
 
 //Icons
 
-import ChatIcon from '@material-ui/icons/Chat'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
+import ChatIcon from "@material-ui/icons/Chat";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 //Redux
 
@@ -27,33 +28,38 @@ import dataReducer from "../redux/reducers/dataReducer";
 
 const styles = {
   card: {
+    position: "relative",
     display: "flex",
     marginBottom: 20,
   },
-  image:{
-      minWidth: 200,
+  image: {
+    minWidth: 200,
   },
-  content:{
-      padding: 25,
-      objectFit: 'cover'
-  }
+  content: {
+    padding: 25,
+    objectFit: "cover",
+  },
 };
 
 export class Post extends Component {
   likedPost = () => {
-    if(this.props.user.likes &&
-       this.props.user.likes.find(like => like.postId === this.props.post.postId))
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find(
+        (like) => like.postId === this.props.post.postId
+      )
+    )
       return true;
     else return false;
   };
   likePost = () => {
     this.props.likePost(this.props.post.postId);
-  }
+  };
   unlikePost = () => {
     this.props.unlikePost(this.props.post.postId);
-  }
+  };
   render() {
-      dayjs.extend(relativeTime)
+    dayjs.extend(relativeTime);
     const {
       classes,
       post: {
@@ -66,43 +72,60 @@ export class Post extends Component {
         commentCount,
       },
       user: {
-        authenticated
-      }
+        authenticated,
+        credentials: { handle },
+      },
     } = this.props;
     const likeButton = !authenticated ? (
       <MyButton tip="Like">
         <Link to="/login">
-          <FavoriteBorder color="primary"/>
+          <FavoriteBorder color="primary" />
         </Link>
       </MyButton>
+    ) : this.likedPost() ? (
+      <MyButton tip="Unlike" onClick={this.unlikePost}>
+        <FavoriteIcon color="primary" />
+      </MyButton>
     ) : (
-      this.likedPost() ? (
-        <MyButton tip="Unlike" onClick={this.unlikePost}>
-          <FavoriteIcon color="primary"/>
-        </MyButton>
-      ) : (
-        <MyButton tip="Like" onClick={this.likePost}>
-          <FavoriteBorder color="primary"/>
-        </MyButton>
-      )
-    )
+      <MyButton tip="Like" onClick={this.likePost}>
+        <FavoriteBorder color="primary" />
+      </MyButton>
+    );
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeletePost postId={postId} />
+      ) : null;
     return (
       <Card className={classes.card}>
-        <CardMedia image={userImage} title="Profile image" className={classes.image}/>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
         <CardContent className={classes.content}>
-          <Typography variant="h5" component={Link} to={'/users/${userHnadle}'} color="primary">{userHandle}</Typography>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={"/users/${userHnadle}"}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          {deleteButton}
+
           <Typography variant="body2" color="textSecondary">
             {/* {createdAt.toString()} */}
             {/*new Date(createdAt._seconds * 1000).toLocaleDateString("en-US")*/}
             {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
-          { likeButton }
-          <span>{ likeCount }</span>
+
+          {likeButton}
+          <span>{likeCount}</span>
           <MyButton tip="comments">
-            <ChatIcon color="primary"/>
+            <ChatIcon color="primary" />
           </MyButton>
-          <span>{ commentCount } comments</span>
+          <span>{commentCount} comments</span>
         </CardContent>
       </Card>
     );
@@ -114,16 +137,19 @@ Post.propTypes = {
   unlikePost: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-  user: state.user
-})
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
 
 const mapActionsToProps = {
   likePost,
-  unlikePost
-}
+  unlikePost,
+};
 
-export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Post));
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Post));
